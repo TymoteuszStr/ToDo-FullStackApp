5cdf16163ea6377b9ce9d6fc24d3f826a780e4c0
Tymek <tymoteusz.strozniak@gmail.com>
2022-01-06 19:56:20 +0100

    add login form

--- frontend/src/views/Auth.vue
+++ frontend/src/views/Auth.vue
@@ -1,15 +1,29 @@
 <template>
-  <h1>AUTH VIEW</h1>
+  <div class="app-view">
+    <h1 class="logo">MyToDoes</h1>
+    <AuthModal />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import AuthModal from "@/components/auth/AuthModal.vue";
+
 export default defineComponent({
   name: "Home",
-  components: {},
+  components: { AuthModal },
 });
 </script>
 
 <style scoped lang="scss">
 @import "@/styles/variables.scss";
+.app-view {
+  background: #c9cedd;
+}
+.logo {
+  width: 100%;
+  text-align: center;
+  font-family: "Share Tech Mono", monospace;
+  padding: 20px;
+}
 </style>
