11a9b038401fc8dd365280485aab5143b345fd4e
Tymoteusz <tymoteusz.strozniak@futurum.tech>
2022-01-07 12:55:30 +0100

    add bg-svg in auth view

--- frontend/src/components/auth/AuthModal.vue
+++ frontend/src/components/auth/AuthModal.vue
@@ -1,33 +1,42 @@
+<!-- @format -->
+
 <template>
-  <div class="auth-modal"><AuthForm /></div>
+	<div class="auth-modal bg-svg">
+		<AuthForm />
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import AuthForm from "./AuthForm.vue";
+	import { defineComponent } from "vue"
+	import AuthForm from "./AuthForm.vue"
 
-export default defineComponent({
-  name: "AuthModal",
-  components: { AuthForm },
-  setup() {
-    return {};
-  },
-});
+	export default defineComponent({
+		name: "AuthModal",
+		components: { AuthForm },
+		setup() {
+			return {}
+		},
+	})
 </script>
 
 <style lang="scss" scoped>
-@import "@/styles/variables.scss";
+	@import "@/styles/variables.scss";
 
-.auth-modal {
-  position: fixed;
-  bottom: -1px;
-  left: -50%;
-  width: 200%;
-  height: 80%;
-  border-radius: 50% 50% 0 0;
-  background: $navy-blue;
-  display: flex;
-  align-items: flex-end;
-  justify-content: center;
-}
+	.auth-modal {
+		position: fixed;
+		bottom: 0px;
+		left: 0;
+		width: 100vw;
+		height: 80%;
+		display: flex;
+		align-items: flex-start;
+		justify-content: center;
+		padding-top: 150px;
+	}
+	.bg-svg {
+		background-image: url("../../assets/backgrounds/auth-bg.svg");
+		background-repeat: no-repeat;
+		background-size: cover;
+		width: 100%;
+	}
 </style>
