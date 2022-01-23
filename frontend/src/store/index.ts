e62cf1e14681960d70bfab6a1524ff04a2c1181a
Tymek <tymoteusz.strozniak@gmail.com>
2022-01-23 13:14:50 +0100

    add vuex-persis

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,18 @@
 import { createStore } from "vuex";
 import taskModule, { TasksState } from "./modules/task.module";
 import userModule, { UserState } from "./modules/user.module";
+import VuexPersistence from 'vuex-persist'
 
 export interface State {
   tasksState: TasksState;
-  UserState: UserState;
+  userState: UserState;
 }
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
+
 export default createStore({
+  plugins: [vuexLocal.plugin],
   modules: { taskModule, userModule },
 });
